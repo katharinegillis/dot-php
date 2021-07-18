e93703e61b821cb595c06ac1c9b732463458bc48
Katharine Cordes <gillis.katharine@gmail.com>
2021-07-18 17:54:52 -0600

    chore: fix comments

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Pull latest php related containers
+# Pull latest and 7.4 php related images
 docker pull php:cli-alpine
 docker pull php:7.4-cli-alpine
 docker pull composer
