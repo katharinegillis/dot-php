205fc4cdc0f2e5f37e49db5abee1da122093778e
Katharine Cordes <gillis.katharine@gmail.com>
2022-08-19 00:10:01 -0600

    refactor: switch to dockerized php
    Re-add the original dockerized php scripts and commands.
    
    Repo: dot-php
    Refs: DF-5

--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# Uninstall php using apt
-sudo apt remove php8.0 php8.0-curl php8.0-xml -y
-
-# Uninstall composer
-sudo rm /usr/local/bin/composer
+# Remove latest php related images
+docker image rm php:cli-alpine
+docker image rm composer
