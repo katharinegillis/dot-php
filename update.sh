ec305393f220fd7f0148bd2a03ea3f7083c0f09e
Katharine Cordes <gillis.katharine@gmail.com>
2022-08-19 00:10:01 -0600

    refactor: switch to dockerized php
    Re-add the original dockerized php scripts and commands.
    
    Repo: dot-php
    Refs: DF-5

--- update.sh
+++ update.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# Update php using apt
-sudo apt install php8.0 php8.0-curl php8.0-xml -y
-
-# Update composer using its self-update
-sudo composer self-update
+# Pull latest php related images
+docker pull php:cli-alpine
+docker pull composer
