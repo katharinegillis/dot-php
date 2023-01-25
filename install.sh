869596b5f24b759e30852a13e63547486b55250e
Katharine Cordes <gillis.katharine@gmail.com>
2023-01-25 00:19:06 -0700

    refactor: local php install

--- install.sh
+++ install.sh
@@ -4,4 +4,20 @@ sudo apt install software-properties-common -y
 sudo add-apt-repository ppa:ondrej/php -y
 sudo apt update -y
 
-sudo apt install php8.2-cli php8.2-mbstring php8.2-dev -y
+sudo apt install php8.2-cli php8.2-mbstring php8.2-dev php8.2-xdebug php8.2-curl -y
+
+EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
+then
+    echo 'ERROR: Invalid installer checksum'
+    rm composer-setup.php
+    exit
+fi
+
+php composer-setup.php --quiet
+rm composer-setup.php
+
+sudo mv composer.phar /usr/local/bin/composer
--- uninstall.sh
+++ uninstall.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-sudo apt remove php8.2-cli php8.2-mbstring php8.2-dev -y
+sudo apt remove php8.2-cli php8.2-mbstring php8.2-dev php8.2-xdebug php8.2-curl -y
+sudo rm -rf /usr/local/bin/composer
+
