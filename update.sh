5e55329e38028f68f41b82b744327ffc819ad291
Katharine Cordes <katharinegillis@users.noreply.github.com>
2025-07-17 16:47:46 -0600

    feat: update update script for 8.4

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ sudo apt install software-properties-common -y
 sudo add-apt-repository ppa:ondrej/php -y
 sudo apt update -y
 
-sudo apt install php8.2-cli php8.2-mbstring php8.2-dev php8.2-xdebug php8.2-curl -y
+sudo apt install php8.4-cli php8.4-mbstring php8.4-dev php8.4-xdebug php8.4-curl -y
 sudo apt-get -y autoremove
 
 EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
